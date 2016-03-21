ae82b1941711143a7ba747e4f3cfd918ac410b1a
vagrant <vagrant@homestead>
2016-03-21 12:03:07 +0000

    bug in migration class name 3

--- database/migrations/2016_02_11_090959_create_password_otp_reset_tabel.php
+++ database/migrations/2016_02_11_090959_create_password_otp_reset_tabel.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordOtpResetTable extends Migration
+class CreatePasswordOtpResetTabel extends Migration
 {
     /**
      * Run the migrations.
