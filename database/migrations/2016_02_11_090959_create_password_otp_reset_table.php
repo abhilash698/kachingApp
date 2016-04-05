46d526464c23842689df72bbeef9948c71ce97fe
vagrant <vagrant@homestead>
2016-04-05 12:34:45 +0000

    bug otp resets 1

--- database/migrations/2016_02_11_090959_create_password_otp_reset_table.php
+++ database/migrations/2016_02_11_090959_create_password_otp_reset_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordOtpResetTabel extends Migration
+class CreatePasswordOtpResetTable extends Migration
 {
     /**
      * Run the migrations.
